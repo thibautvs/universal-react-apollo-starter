98513b0a58660ac6c7ac8f9b9b27b084723dda18
Thibaut Van Spaandonck <thibaut@urge2code.com>
2018-06-17 13:01:32 +0200

    Implemented 404 page

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,8 +1,8 @@
-import React, { Component } from 'react'
+import React from 'react'
 import styled from 'styled-components'
 import Helmet from 'react-helmet'
 import { Switch, Route } from 'react-router-dom'
-import { Link, NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import '../vendor/foundation.css'
 
 const StyledHeader = styled.h1`
@@ -21,99 +21,78 @@ const GridContainer = styled.div`
   }
 `
 
-class Menu extends Component {
-  render() {
-    return (
-      <div>
-        <ul>
-          <li>
-            <NavLink exact to={'/'}>
-              Homepage
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to={'/about'}>About</NavLink>
-          </li>
-          <li>
-            <NavLink to={'/contact'}>Contact</NavLink>
-          </li>
-        </ul>
-      </div>
-    )
-  }
-}
+const Menu = () => (
+  <article>
+    <ul>
+      <li>
+        <NavLink exact to={'/'}>
+          Homepage
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to={'/about'}>About</NavLink>
+      </li>
+    </ul>
+  </article>
+)
 
-class Homepage extends Component {
-  render() {
-    return (
-      <div>
-        <Helmet title="Home" />
-        <Menu />
-        <StyledHeader>Homepage</StyledHeader>
-        <p>Zurb Foundation Grid demo</p>
-        <GridContainer>
-          <div className="grid-x">
-            <div className="cell">full width cell</div>
-            <div className="cell">full width cell</div>
-          </div>
-          <div className="grid-x">
-            <div className="cell small-6">6 cells</div>
-            <div className="cell small-6">6 cells</div>
-          </div>
-          <div className="grid-x">
-            <div className="cell medium-6 large-4">12/6/4 cells</div>
-            <div className="cell medium-6 large-8">12/6/8 cells</div>
-          </div>
-        </GridContainer>
+const HomePage = () => (
+  <section>
+    <Helmet title="Home" />
+    <Menu />
+    <StyledHeader>Homepage</StyledHeader>
+    <p>Zurb Foundation Grid demo</p>
+    <GridContainer>
+      <div className="grid-x">
+        <div className="cell">full width cell</div>
+        <div className="cell">full width cell</div>
       </div>
-    )
-  }
-}
+      <div className="grid-x">
+        <div className="cell small-6">6 cells</div>
+        <div className="cell small-6">6 cells</div>
+      </div>
+      <div className="grid-x">
+        <div className="cell medium-6 large-4">12/6/4 cells</div>
+        <div className="cell medium-6 large-8">12/6/8 cells</div>
+      </div>
+    </GridContainer>
+  </section>
+)
 
-class About extends Component {
-  render() {
-    return (
-      <div>
-        <Helmet title="About" />
-        <Menu />
-        <StyledHeader>About</StyledHeader>
-      </div>
-    )
-  }
-}
+const AboutPage = () => (
+  <section>
+    <Helmet title="About" />
+    <Menu />
+    <StyledHeader>About</StyledHeader>
+  </section>
+)
 
-class Contact extends Component {
-  render() {
-    return (
-      <div>
-        <Helmet title="Contact" />
-        <Menu />
-        <StyledHeader>Contact</StyledHeader>
-      </div>
-    )
-  }
-}
+const NotFoundPage = () => (
+  <section>
+    <Helmet title="Not found" />
+    <Menu />
+    <header>404</header>
+  </section>
+)
 
-export default class App extends Component {
-  render() {
-    return (
-      <div>
-        <Helmet
-          htmlAttributes={{ lang: 'en', amp: undefined }} // amp takes no value
-          titleTemplate="Universal React Apollo Starter | %s"
-          titleAttributes={{ itemprop: 'name', lang: 'en' }}
-          meta={[
-            { name: 'description', content: 'Universal React Apollo Starter' },
-            { name: 'viewport', content: 'width=device-width, initial-scale=1' }
-          ]}
-          link={[{ rel: 'stylesheet', href: '/dist/styles.css' }]}
-        />
-        <Switch>
-          <Route exact path="/" component={Homepage} />
-          <Route path="/about" component={About} />
-          <Route path="/contact" component={Contact} />
-        </Switch>
-      </div>
-    )
-  }
-}
+const App = () => (
+  <div>
+    <Helmet
+      htmlAttributes={{ lang: 'en', amp: undefined }} // amp takes no value
+      titleTemplate="Universal React Apollo Starter | %s"
+      titleAttributes={{ itemprop: 'name', lang: 'en' }}
+      meta={[
+        { name: 'description', content: 'Universal React Apollo Starter' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+      ]}
+      link={[{ rel: 'stylesheet', href: '/dist/styles.css' }]}
+    />
+    <Switch>
+      <Route exact path="/" component={HomePage} />
+      <Route path="/about" component={AboutPage} />
+      <Route component={NotFoundPage} />
+    </Switch>
+  </div>
+)
+
+export default App
