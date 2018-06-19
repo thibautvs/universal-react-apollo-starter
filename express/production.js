c1796241619fc9be5d09055e4fafc0e1a69c065f
Thibaut Van Spaandonck <thibaut@urge2code.com>
2018-06-19 09:39:30 +0200

    Configured gzip and removed x-powered-by in prod server

--- express/production.js
+++ express/production.js
@@ -3,6 +3,7 @@
 /* eslint-disable import/no-dynamic-require */
 const express = require('express')
 const path = require('path')
+const compression = require('compression')
 const ClientStatsPath = path.join(__dirname, './../dist/stats.json')
 const ServerRendererPath = path.join(__dirname, './../dist/server.js')
 const ServerRenderer = require(ServerRendererPath).default
@@ -13,6 +14,8 @@ const PORT = process.env.PORT || 3000
 
 app.use('/dist', express.static(path.join(__dirname, '../dist')))
 app.use(ServerRenderer(Stats))
+app.use(compression())
+app.disable('x-powered-by')
 
 app.listen(PORT, error => {
   if (error) {
